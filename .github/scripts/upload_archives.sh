9e3b1e2ea27ae37d6b9be814e800a3bd637f1317
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-09-15 15:43:25 +0200

    Make upload resilient to temporary network failures

--- .github/scripts/upload_archives.sh
+++ .github/scripts/upload_archives.sh
@@ -12,5 +12,12 @@ tag=$(mix xla.release_tag)
 cd cache/build
 
 for file in *.tar.gz; do
-  gh release upload --clobber $tag $file
+  # Uploading is the final action after several hour long build,
+  # so in case of any temporary network failures we want to retry
+  # a number of times
+  for i in {1..10}; do
+    gh release upload --clobber $tag $file && break
+    echo "Upload failed, retrying in 30s"
+    sleep 30
+  done
 done
