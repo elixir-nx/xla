ddd958f279506c19bd8609ca58a3fdaff1f5fdd0
Sean Moriarity <smoriarity.5@gmail.com>
2022-12-12 16:48:29 -0800

    Be smarter about applying alpine build

---
+++ patch_tensorflow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+tmpf=/tmp/musl.log
+
+# detect if on musl and apply patch if we are
+libc=$(ldd /bin/ls | grep 'musl' | head -1 | cut -d ' ' -f1)
+if [ ! -z $libc ]; then
+  git apply patch_tensorflow.sh
+fi
