7e16e4f9d487b053df558f8282877bcbcdb4f766
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-12-15 18:42:30 +0100

    Include patch files in the release

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule XLA.MixProject do
       links: %{
         "GitHub" => "https://github.com/elixir-nx/xla"
       },
-      files: ~w(extension lib Makefile Makefile.win mix.exs README.md LICENSE CHANGELOG.md)
+      files:
+        ~w(extension lib Makefile Makefile.win mix.exs README.md LICENSE CHANGELOG.md patch_tensorflow.sh tensorflow-alpine.patch)
     ]
   end
 
