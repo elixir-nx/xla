cf2c6ff0768dbdee88dc276b24d18e570497537a
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-08-09 17:44:03 +0200

    Up

--- .github/scripts/compile_unless_exists.sh
+++ .github/scripts/compile_unless_exists.sh
@@ -9,15 +9,15 @@ mix compile
 
 tag=$(mix xla.info release_tag)
 
-if gh release list | grep $tag; then
-  archive_filename=$(mix xla.info archive_filename)
+# if gh release list | grep $tag; then
+#   archive_filename=$(mix xla.info archive_filename)
 
-  if gh release view $tag | grep $archive_filename; then
-    echo "Found $archive_filename in $tag release artifacts, skipping compilation"
-  else
+#   if gh release view $tag | grep $archive_filename; then
+#     echo "Found $archive_filename in $tag release artifacts, skipping compilation"
+#   else
     XLA_BUILD=true mix compile
-  fi
-else
-  echo "::error::Release $tag not found"
-  exit 1
-fi
+#   fi
+# else
+#   echo "::error::Release $tag not found"
+#   exit 1
+# fi
