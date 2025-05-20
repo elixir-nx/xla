129b255bc0a08d48986cb6ad00e5eaccadf6e9ba
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-05-20 13:25:37 +0200

    Up

--- lib/xla.ex
+++ lib/xla.ex
@@ -342,7 +342,8 @@ defmodule XLA do
       "--copt=-Wno-error=unused-command-line-argument",
       # See https://github.com/jax-ml/jax/blob/0842cc6f386a20aa20ed20691fb78a43f6c4a307/.bazelrc#L127-L138
       "--copt=-Wno-gnu-offsetof-extensions",
-      "--copt=-Qunused-arguments"
+      "--copt=-Qunused-arguments",
+      "--copt=-Wno-error=c23-extensions"
     ]
 
     bazel_build_flags =
