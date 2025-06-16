61703fac5590ed8ea16f2fd5025b2a1761437770
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-06-16 09:19:58 +0200

    Remove fixed arm patch

--- extension/patches/apply.sh
+++ extension/patches/apply.sh
@@ -5,11 +5,9 @@ set -ex
 dir="$(cd "$(dirname "$0")"; pwd)"
 arch="$(uname -m)"
 
-if [[ $arch == 'aarch64' ]]; then
-  # See https://github.com/tensorflow/tensorflow/issues/62490#issuecomment-2077646521
-  git apply $dir/absl_workspace.patch
-  cp $dir/absl_neon.patch third_party/tsl/third_party/absl
-fi
+# if [[ $arch == 'aarch64' ]]; then
+#   # ...
+# fi
 
 # XLA build links againast a major version of CUDA libraries, so the
 # build should be compatible with CUDA installations across all minor
