8b0c7a5fa84b7b759c20b8354a67fcf96c58c6d1
Jonatan Kłosko <jonatanklosko@gmail.com>
2026-01-27 19:00:34 +0100

    Move custom calls from exla to xla

---
+++ extension/custom_calls/qr_bf16.cc
@@ -0,0 +1,20 @@
+#include "qr.h"
+
+#include "xla/tsl/platform/types.h"
+
+ffi::Error qr_cpu_custom_call_bf16_impl(ffi::Buffer<ffi::BF16> operand,
+                                        ffi::ResultBuffer<ffi::BF16> q,
+                                        ffi::ResultBuffer<ffi::BF16> r) {
+  return qr_cpu_custom_call_impl<tsl::bfloat16, ffi::Buffer<ffi::BF16>>(
+      operand, q, r);
+}
+
+XLA_FFI_DEFINE_HANDLER_SYMBOL(qr_cpu_custom_call_bf16,
+                              qr_cpu_custom_call_bf16_impl,
+                              ffi::Ffi::Bind()
+                                  .Arg<ffi::Buffer<ffi::BF16>>()
+                                  .Ret<ffi::Buffer<ffi::BF16>>()
+                                  .Ret<ffi::Buffer<ffi::BF16>>());
+
+XLA_FFI_REGISTER_HANDLER(ffi::GetXlaFfiApi(), "qr_cpu_custom_call_bf16", "Host",
+                         qr_cpu_custom_call_bf16);
---
+++ extension/custom_calls/qr_f16.cc
@@ -0,0 +1,20 @@
+#include "qr.h"
+
+#include "Eigen/Core"
+
+ffi::Error qr_cpu_custom_call_f16_impl(ffi::Buffer<ffi::F16> operand,
+                                       ffi::ResultBuffer<ffi::F16> q,
+                                       ffi::ResultBuffer<ffi::F16> r) {
+  return qr_cpu_custom_call_impl<Eigen::half, ffi::Buffer<ffi::F16>>(operand,
+                                                                     q, r);
+}
+
+XLA_FFI_DEFINE_HANDLER_SYMBOL(qr_cpu_custom_call_f16,
+                              qr_cpu_custom_call_f16_impl,
+                              ffi::Ffi::Bind()
+                                  .Arg<ffi::Buffer<ffi::F16>>()
+                                  .Ret<ffi::Buffer<ffi::F16>>()
+                                  .Ret<ffi::Buffer<ffi::F16>>());
+
+XLA_FFI_REGISTER_HANDLER(ffi::GetXlaFfiApi(), "qr_cpu_custom_call_f16", "Host",
+                         qr_cpu_custom_call_f16);
